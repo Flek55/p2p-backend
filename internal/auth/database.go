a6860a62b6b64968c0c2fc6d26c816db8dc16d4d
Arseniy <arseniy.burtsev@gmail.com>
2025-05-29 22:10:55 +0300

    fix

--- internal/auth/database.go
+++ internal/auth/database.go
@@ -1,17 +1,23 @@
 package auth
 
 import (
-	"fmt"
 	"log"
-	"time"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var database *gorm.DB
 
+func initDsn() string {
+	return "host=" + os.Getenv("host") + "port=" + os.Getenv("port") + "user=" + os.Getenv("user") + "dbname=" + os.Getenv("dbname") + "password=" + os.Getenv("password") + "sslmode=disable"
+}
+
 func InitDb() *gorm.DB {
-	dsn := "host=localhost port=5432 user=emilkerimov dbname=postgres password=123 sslmode=disable"
+	godotenv.Load()
+	dsn := initDsn()
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)

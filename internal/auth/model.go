4b7aa3307664f11ed5d9be75dbf1e825cb747468
Emil <kerimovemil187@gmail.com>
2025-05-29 21:34:58 +0300

    normal gorm

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -7,18 +7,18 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID `gorm:"type:uuid;primary_key"`
-	Email        string    `gorm:"uniqueIndex;not null"`
-	PasswordHash string    `gorm:"not null"`
-	CreatedAt    time.Time
+	ID           uuid.UUID `json:"id" gorm:"primaryKey"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"password_hash"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 type Session struct {
-	ID           uuid.UUID
-	UserID       uuid.UUID
-	RefreshToken string
-	UserAgent    string
-	IPAddress    string
-	ExpiresAt    time.Time
-	CreatedAt    time.Time
+	ID           uuid.UUID `json:"id" gorm:"primaryKey"`
+	UserID       uuid.UUID `json:"user_id"`
+	RefreshToken string    `json:"refresh_token"`
+	UserAgent    string    `json:"user_agent"`
+	IPAddress    string    `json:"ip_address"`
+	ExpiresAt    time.Time `json:"expires_at"`
+	CreatedAt    time.Time `json:"created_at"`
 }

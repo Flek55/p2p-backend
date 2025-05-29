5383721a412934faa6a9639583b641407bd819fd
Emil <kerimovemil187@gmail.com>
2025-05-29 22:02:30 +0300

    getDb

--- internal/auth/database.go
+++ internal/auth/database.go
@@ -23,11 +23,7 @@ func InitDb() *gorm.DB {
 func getDB() *gorm.DB {
 	if database == nil {
 		database = InitDb()
-		sleep := time.Duration(1)
 		for database == nil {
-			sleep = sleep * 2
-			fmt.Printf("Waiting for database connection %d seconds\n", sleep)
-			time.Sleep(sleep * time.Second)
 			database = InitDb()
 		}
 	}

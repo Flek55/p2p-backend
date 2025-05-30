53843fab66600567a6a54f7893a531afea745eaf
Emil <kerimovemil187@gmail.com>
2025-05-31 00:47:05 +0300

    first api methods

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,16 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"time"
-
+	"github.com/Flek55/p2p-backend/handlers"
 	"github.com/Flek55/p2p-backend/internal/auth"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	ctx := context.Background()
-
 	auth.InitDb()
 
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	s := auth.CreateService("hui", 300)
-
-	err := s.Register(ctx, "burito1533@gmail.com", "zhara")
-
-	if err != nil {
-		fmt.Println("error creating user: ", err)
-	}
-
 	r := gin.Default()
+	r.POST("/login", handlers.LoginUser)
+	r.POST("/register", handlers.RigisterUser)
 	r.Run()
 }
---
+++ handlers/actions.go
@@ -0,0 +1,69 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Flek55/p2p-backend/internal/auth"
+	"github.com/gin-gonic/gin"
+)
+
+var service *auth.Service
+
+func init() {
+	service = auth.CreateService("hui", 300)
+}
+
+type LoginAndRegisterRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func LoginUser(c *gin.Context) {
+	var req LoginAndRegisterRequest
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid request body",
+		})
+		return
+	}
+
+	userAgent := c.Request.UserAgent()
+	ip := c.ClientIP()
+	ctx := c.Request.Context()
+
+	accessToken, refreshToken, err := service.Login(ctx, req.Email, req.Password, userAgent, ip)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message":      "Login successful",
+		"accessToken":  accessToken,
+		"refreshToken": refreshToken,
+	})
+}
+
+func RigisterUser(c *gin.Context) {
+	var req LoginAndRegisterRequest
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid request body",
+		})
+		return
+	}
+
+	err := service.Register(c.Request.Context(), req.Email, req.Password)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "User created successfully",
+	})
+}
